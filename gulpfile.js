3b6d1120214e77e59b61611d7d3a33acf95f97e0
fantazer <fantazer@text.ru>
2015-07-21 19:21:53 +0300

    Compress image + adaptive slider

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 var gulp = require("gulp"),
-	wiredep = require('wiredep').stream,
-	autoprefixer = require('gulp-autoprefixer'),
-	useref = require('gulp-useref'),
-	gulpif = require('gulp-if'),
-	uglify = require('gulp-uglify'),
-	minifyCss = require('gulp-minify-css');
+    wiredep = require('wiredep').stream,
+    autoprefixer = require('gulp-autoprefixer'),
+    useref = require('gulp-useref'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    minifyCss = require('gulp-minify-css'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    imageminJpegtran = require('imagemin-jpegtran'),
+    jpegoptim = require('imagemin-jpegoptim'),
+    imageminMozjpeg = require('imagemin-mozjpeg');
  
 //Prefix my css
 gulp.task('prefix', function () {
@@ -14,6 +19,29 @@ gulp.task('prefix', function () {
         }))
         .pipe(gulp.dest('app/css/'));
 });
+
+//Compress img
+gulp.task('imagePng',function(){
+            return gulp.src('app/img/*.png')
+                .pipe(imagemin({
+                    progressive: true,
+                    svgoPlugins: [{removeViewBox: false}],
+                    use: [pngquant({quality: '40', speed: 4})]
+                }))
+                .pipe(gulp.dest('dist/img/'));
+});
+
+//Compress imgJpg
+gulp.task('imageJpg',function(){
+            return gulp.src('app/img/*.jpg')
+            .pipe(imagemin({
+                    progressive: true,
+                    svgoPlugins: [{removeViewBox: false}],
+                    use: [imageminMozjpeg({quality: '60', speed: 11})]
+                }))
+            .pipe(gulp.dest('dist/img/'));
+});
+
 
 //wiredep
 gulp.task('bower', function () {
@@ -39,4 +67,5 @@ gulp.task('html', function () {
 
 
 //default
-gulp.task('use',[ 'prefix' , 'bower'])
+gulp.task('use',[ 'prefix' , 'bower']);
+gulp.task('img',[ 'imagePng' , 'imageJpg']);
